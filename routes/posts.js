b0af538bc2b11512f478070472bfe48e5a2b811b
ZachRochette <zachrochette@hotmail.com>
2022-05-25 03:01:49 -0600

    finished most of the backend

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-undef */
 const router = require("express").Router();
-const res = require("express/lib/response");
 const Post = require("../models/Post");
+const User = require("../models/User");
 //create a post
 
 router.post("/", async (req, res) => {
@@ -76,16 +75,15 @@ router.get("/:id", async (req, res) => {
 
 //get timeline posts
 
-router.get("/timeline", async (req, res) => {
+router.get("/timeline/all", async (req, res) => {
   try {
     const currentUser = await User.findById(req.body.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.following.map((friendId) => {
-        Post.find({ userId: friendId });
+        return Post.find({ userId: friendId });
       })
     );
-
     res.json(userPosts.concat(...friendPosts));
   } catch (err) {
     res.status(500).json(err);
