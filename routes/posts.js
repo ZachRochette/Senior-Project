a5469f62995d79139ad00d0c8be66dd33ac97312
ZachRochette <zachrochette@hotmail.com>
2022-05-27 02:04:44 -0600

    commit

--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,7 @@ router.post("/", async (req, res) => {
   try {
     const savedPost = await newPost.save();
     res.status(200).json("savedPost");
-  } catch {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
